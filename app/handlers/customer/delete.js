367eb2fb0a119c1deb42759d7eb6bc605263c132
Vaibhav Jain <jaivai@amazon.com>
2023-03-13 10:39:36 -0400

    Added changes to delete claims data

--- app/handlers/customer/delete.js
+++ app/handlers/customer/delete.js
@@ -13,7 +13,7 @@ const { unmarshall } = require("@aws-sdk/util-dynamodb");
 const ddbClient = new DynamoDBClient({ region: process.env.AWS_REGION });
 
 exports.handler = async function (event, context) {
-
+  let deleteResp = {};
   const findCustomerParams = {
     FilterExpression: "SK = :sk AND cognitoIdentityId = :identifier",
     ExpressionAttributeValues: {
@@ -23,19 +23,18 @@ exports.handler = async function (event, context) {
     TableName: process.env.CUSTOMER_TABLE_NAME,
   };
 
-  let custDetails = {};
   const { Items: idRecords } = await ddbClient.send(
     new ScanCommand(findCustomerParams)
   );
 
   if (idRecords && idRecords.length > 0) {
     const uItem = unmarshall(idRecords["0"]);
-    const PK = uItem.PK;
+    const customerUUID = uItem.PK;
 
     const getCustDetailsInput = {
       KeyConditionExpression: "PK = :pk",
       ExpressionAttributeValues: {
-        ":pk": { S: PK },
+        ":pk": { S: customerUUID },
       },
       TableName: process.env.CUSTOMER_TABLE_NAME,
     };
@@ -45,17 +44,20 @@ exports.handler = async function (event, context) {
     );
 
     const deleteCustReqs = [];
+    const deletePolicyReqs = [];
+    const deleteClaimReqs = [];
+
 
     customerRecords.forEach((customerRecord) => {
-      const uCustRecd = unmarshall(customerRecord);
+      const uCustRecord = unmarshall(customerRecord);
       const delCustReq = {
         DeleteRequest: {
           Key: {
             PK: {
-              S: PK,
+              S: customerUUID,
             },
             SK: {
-              S: uCustRecd.SK,
+              S: uCustRecord.SK,
             },
           },
         },
@@ -63,21 +65,85 @@ exports.handler = async function (event, context) {
       deleteCustReqs.push(delCustReq);
     });
 
+    const findCustomerPoliciesInput = {
+      FilterExpression: "SK = :sk",
+      ExpressionAttributeValues: {
+        ":sk": { S: `Customer|${customerUUID}` },
+      },
+      TableName: process.env.POLICY_TABLE_NAME,
+    };
+
+    const { Items: policies } = await ddbClient.send(
+      new ScanCommand(findCustomerPoliciesInput)
+    );
+
+    policies.forEach((policy) => {
+      const uPolicy = unmarshall(policy);
+      const delPolicyReq = {
+        DeleteRequest: {
+          Key: {
+            PK: {
+              S: uPolicy.PK,
+            },
+            SK: {
+              S: uPolicy.SK,
+            },
+          },
+        },
+      };
+      deletePolicyReqs.push(delPolicyReq);
+    });
+
+    const findCustomerClaimsInput = {
+      FilterExpression: "SK = :sk",
+      ExpressionAttributeValues: {
+        ":sk": { S: `Customer|${customerUUID}` },
+      },
+      TableName: process.env.CLAIMS_TABLE_NAME,
+    };
+
+    const { Items: claims } = await ddbClient.send(
+      new ScanCommand(findCustomerClaimsInput)
+    );
+
+    claims.forEach((claim) => {
+      const uClaim = unmarshall(claim);
+      const delClaimReq = {
+        DeleteRequest: {
+          Key: {
+            PK: {
+              S: uClaim.PK,
+            },
+            SK: {
+              S: uClaim.SK,
+            },
+          },
+        },
+      };
+      deleteClaimReqs.push(delClaimReq);
+    });
+
+    
+
     const batchWriteCmdInput = {
       RequestItems: {
         [process.env.CUSTOMER_TABLE_NAME]: deleteCustReqs,
+        [process.env.POLICY_TABLE_NAME]: deletePolicyReqs,
+        [process.env.CLAIMS_TABLE_NAME]: deleteClaimReqs,
       },
     };
 
-    const deleteResp = await ddbClient.send(
+    console.log('batchWriteCmdInput = ', batchWriteCmdInput)
+
+    deleteResp = await ddbClient.send(
       new BatchWriteItemCommand(batchWriteCmdInput)
     );
 
-    
+    console.log('deleteResp = ', deleteResp)
   }
   return {
     statusCode: 200,
-    body: JSON.stringify(custDetails),
+    body: JSON.stringify(deleteResp),
     headers: {
       "content-type": "application/json",
       "Access-Control-Allow-Origin": "*",
